351052af801f6267df43d824cee3a393f7f8ce2e
Denise <denisestanzione06@gmail.com>
2024-12-05 16:23:03 +0100

    2.0

---
+++ src/Torneo.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+
+/**
+ *
+ * @author Luigi
+ */
+public class Torneo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Inizio torneo");
+        // TODO code application logic here
+    }
+    
+}
